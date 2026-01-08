9714806afb79bd6a407fdba35578f74b37521695
thenil3sh <nileshkumar.newdelhi@gmail.com>
2026-01-09 01:13:07 +0530

    waybar, more scripts more cool lmao

---
+++ .config/hypr/scripts/feh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MAX_WIDTH=1200
+MAX_HEIGHT=800
+
+IMG="$1"
+
+read IMG_WIDTH IMG_HEIGHT <<<"$(identify -format "%w %h" "$IMG")" 
+read SCALED_WIDTH SCALED_HEIGHT <<<"$(feh-clamp $IMG_WIDTH $IMG_HEIGHT)" &&
+echo "$SCALED_WIDTH $SCALED_HEIGHT"
+
+feh \
+  --geometry "$SCALED_WIDTH"x"$SCALED_HEIGHT" \
+  --auto-zoom \
+  --scale-down \
+  --image-bg black \
+  "$IMG"
--- .config/waybar/record_state.sh
+++ .config/waybar/record_state.sh
@@ -3,5 +3,5 @@ if pgrep -x 'wf-recorder' > /dev/null;
 then
 printf '{"text": "󰑊", "tooltip": "", "class": "recording", "percentage": "" }'
 else
-printf '{"text": " ", "tooltip": "", "class": "not-recording", "percentage": ""}'
+printf '{"text": "", "tooltip": "", "class": "not-recording", "percentage": ""}'
 fi
