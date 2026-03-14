90cad232e73107149585e668e80aa39ac35f51fa
thenil3sh <nileshkumar.newdelhi@gmail.com>
2026-03-14 09:20:07 +0530

    + tweaky ahh
    + fish_command_not_found is more stable
    - reduced bg transparency, helps with light wallpaper
    + added doro_timer to waybar!!! agh

--- .config/fish/functions/fish_command_not_found.fish
+++ .config/fish/functions/fish_command_not_found.fish
@@ -1,20 +1,33 @@
+#!/usr/bin/fish
 function fish_command_not_found
     set BINARY $argv[1]
-
     set RESULT (pkgfile -b $BINARY 2>/dev/null)
 
     /usr/bin/printf "Command \x1b[1m\x1b[33m'$BINARY'\x1b[0m not found.\n"
+
     if test -n "$RESULT"
-        /usr/bin/printf "\x1b[34mAvailable in : \x1b[0m\n"
-        /usr/bin/echo $RESULT |
-            /usr/bin/sed 's/ /\n/' |
-            /usr/bin/awk -F / '{print "\t\x1b[35m\x1b[1m" $1 "/\x1b[0m\x1b[1m" $2 }'
+        printf "\x1b[34mAvailable in : \x1b[0m\n"
+        echo $RESULT |
+            sed 's/ /\n/' |
+            awk -F / '{print "\t\x1b[35m\x1b[1m" $1 "/\x1b[0m\x1b[1m" $2 }'
+
+        # A temporary function that'll remove itself once called
         function $BINARY --inherit-variable RESULT --inherit-variable BINARY
-            sudo pacman -S $RESULT[1]
+
+            # Edge case, if $BINARY is installed before this function is used
+            if command -v $BINARY >/dev/null 2>&1
+                # Behave normally
+                command $BINARY $argv
+            else
+                sudo pacman -S $RESULT[1]
+            end
+
             /usr/bin/echo ""
             functions --erase $BINARY
         end
+
         /usr/bin/printf "\nRun \x1b[33m$BINARY\x1b[0m again to install \x1b[1m$(echo $RESULT[1] | awk -F / '{print "\x1b[35m\x1b[1m" $1 "/\x1b[0m\x1b[1m" $2 }')\x1b[0m\n"
     end
-    return
+
+    return # 127
 end
